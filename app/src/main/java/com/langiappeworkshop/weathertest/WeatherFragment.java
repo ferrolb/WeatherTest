c6ad8d981d7ff774c5145ee9e89f94146fac1625
ferrolb <ferrol.blackmon@gmail.com>
2018-09-09 14:57:25 -0400

    Added WeatherFragment and its layout to the WeatherActivity.

---
+++ app/src/main/java/com/langiappeworkshop/weathertest/WeatherFragment.java
@@ -0,0 +1,18 @@
+package com.langiappeworkshop.weathertest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_weather, container, false);
+
+        return rootView;
+    }
+}
