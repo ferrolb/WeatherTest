b4f2c644ff772bcaf6e2619a7ec8beaab8c2e0e5
ferrolb <ferrol.blackmon@gmail.com>
2018-09-10 06:23:05 -0400

    Add more sample data

--- app/src/main/java/com/langiappeworkshop/weathertest/WeatherFragment.java
+++ app/src/main/java/com/langiappeworkshop/weathertest/WeatherFragment.java
@@ -108,12 +108,34 @@ public class WeatherFragment extends Fragment {
         Day day = new Day(R.mipmap.ic_launcher, "", "Monday 9/10/2018",
                 "Chance of a Thunderstorm", "Precip: 60%", "Lo: 69", "Hi: 84", "Humid: 76%", "Wind: 7 SW");
         tenDaysWeatherList.add(day);
-        day = new Day(R.mipmap.ic_launcher, "", "Monday 9/11/2018",
+        day = new Day(R.mipmap.ic_launcher, "", "Tuesday 9/11/2018",
                 "Chance of a Thunderstorm", "Precip: 80%", "Lo: 71", "Hi: 87", "Humid: 72", "Wind: 4 WNW");
         tenDaysWeatherList.add(day);
-        day = new Day(R.mipmap.ic_launcher, "", "Monday 9/12/2018",
+        day = new Day(R.mipmap.ic_launcher, "", "Wednesday 9/12/2018",
                 "Chance of a Thunderstorm", "Precip: 40%", "Lo: 71", "Hi: 88", "Humid: 72%", "Wind: 5 ENE");
         tenDaysWeatherList.add(day);
+        day = new Day(R.mipmap.ic_launcher, "", "Thursday 9/13/2018",
+                "Chance of a Thunderstorm", "Precip: 80%", "Lo: 71", "Hi: 87", "Humid: 72", "Wind: 4 WNW");
+        tenDaysWeatherList.add(day);
+        day = new Day(R.mipmap.ic_launcher, "", "Friday 9/14/2018",
+                "Chance of a Thunderstorm", "Precip: 40%", "Lo: 71", "Hi: 88", "Humid: 72%", "Wind: 5 ENE");
+        tenDaysWeatherList.add(day);
+        day = new Day(R.mipmap.ic_launcher, "", "Saturday 9/15/2018",
+                "Chance of a Thunderstorm", "Precip: 80%", "Lo: 71", "Hi: 87", "Humid: 72", "Wind: 4 WNW");
+        tenDaysWeatherList.add(day);
+        day = new Day(R.mipmap.ic_launcher, "", "Sunday 9/16/2018",
+                "Chance of a Thunderstorm", "Precip: 40%", "Lo: 71", "Hi: 88", "Humid: 72%", "Wind: 5 ENE");
+        tenDaysWeatherList.add(day);
+        day = new Day(R.mipmap.ic_launcher, "", "Monday 9/17/2018",
+                "Chance of a Thunderstorm", "Precip: 80%", "Lo: 71", "Hi: 87", "Humid: 72", "Wind: 4 WNW");
+        tenDaysWeatherList.add(day);
+        day = new Day(R.mipmap.ic_launcher, "", "Tuesday 9/18/2018",
+                "Chance of a Thunderstorm", "Precip: 40%", "Lo: 71", "Hi: 88", "Humid: 72%", "Wind: 5 ENE");
+        tenDaysWeatherList.add(day);
+        day = new Day(R.mipmap.ic_launcher, "", "Wednesday 9/19/2018",
+                "Chance of a Thunderstorm", "Precip: 80%", "Lo: 71", "Hi: 87", "Humid: 72", "Wind: 4 WNW");
+        tenDaysWeatherList.add(day);
+
 
         // TODO: put empty/null check for data here later
 
